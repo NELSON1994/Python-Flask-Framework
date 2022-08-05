8d300e28533632a2c1ea8689215fa57972630f38
NELSON1994 <nelson62moses@gmail.com>
2022-08-05 21:05:07 +0300

    flask variables and routing

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,25 @@ from app import app
 @app.route('/index')
 def index():
     return "welcome to your first flask application"
+
+
+@app.route('/first')
+def first():
+    user = {'username': 'Nelson'}
+    return '''
+    <html>
+        <head>
+            <title>Home Page - Flask App</title>
+        </head>
+        <body>
+            <h1>Hello, ''' + user['username'] + '''!</h1>
+            <p>Welcome onboard</p>
+        </body>
+    </html>'''
+
+
+@app.route('/pathvariable/<name>')
+def passPathVariable(name):
+    # for int--> %d.  float--> %f
+    return "my name is : %s" % name
+
