42fd99f57446cfd5220c608f2ee29ffecae07e5b
NELSON1994 <nelson62moses@gmail.com>
2022-09-01 21:14:35 +0300

    sql connection

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,8 @@ from flask_wtf import Form
 from wtforms import IntegerField, TextAreaField, SubmitField, RadioField, SelectField, StringField
 from wtforms.validators import DataRequired
 
+import sqlite3 as sql
+
 
 app = Flask(__name__)
 
@@ -21,6 +23,46 @@ app.secret_key = 'development key'
 from app import app
 
 mail = Mail(app)
+
+@app.route('/enternew')
+def new_student():
+   return render_template('student.html')
+
+
+@app.route('/addrec', methods=['POST', 'GET'])
+def addrec():
+    if request.method == 'POST':
+        try:
+            nm = request.form['nm']
+            addr = request.form['add']
+            city = request.form['city']
+            pin = request.form['pin']
+
+            with sql.connect("database.db") as con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO students (name,addr,city,pin) VALUES(?, ?, ?, ?)",(nm,addr,city,pin) )
+
+                con.commit()
+                msg = "Record successfully added"
+        except:
+            con.rollback()
+            msg = "error in insert operation"
+
+        finally:
+            return render_template("results.html", msg=msg)
+            con.close()
+
+
+@app.route('/list')
+def list():
+    con = sql.connect("database.db")
+    con.row_factory = sql.Row
+
+    cur = con.cursor()
+    cur.execute("select * from students")
+
+    rows = cur.fetchall();
+    return render_template("list.html", rows=rows)
 
 @app.route("/email")
 def emailsending():
@@ -199,3 +241,8 @@ def contact():
         #     return render_template('success.html')
         # elif request.method == 'GET':
         # return render_template('contact.html', form=form)
+
+
+@app.route('/home')
+def home():
+   return render_template('home.html')
